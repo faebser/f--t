7a6251133b4942c74a5d00f0e2aef64bc4dc2d61
David Huser <dafhus@gmail.com>
2014-02-10 15:15:31 +0100

    write content to file

--- fbgroupscraper/contentdownloader.py
+++ fbgroupscraper/contentdownloader.py
@@ -1,4 +1,5 @@
 from facepy import GraphAPI
+import json
 
 group_id = "290537284306690"
 access_token = "YOUR_ACCESS_TOKEN"
@@ -8,4 +9,5 @@ graph = GraphAPI(access_token)
 # https://facepy.readthedocs.org/en/latest/usage/graph-api.html
 data = graph.get(group_id + "/feed", page=False, retry=3, limit=800)
 
-print data
+with open('content.json', 'w') as outfile:
+  json.dump(data, outfile, indent = 4)
